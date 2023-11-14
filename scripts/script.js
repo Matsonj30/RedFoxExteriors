df2ec0737614b29d233fc2072371c05875984c34
Matsonj30 <matsonj2013@gmail.com>
2023-11-13 17:19:43 -0700

    scroll fix

--- scripts/script.js
+++ scripts/script.js
@@ -29,7 +29,7 @@ function scrollToDiv(location, pageURL = null){
         document.getElementById(location).scrollIntoView({behavior: "smooth", block: "center"});
     } //On correct page
     else{
-        window.location.href = 'http://127.0.0.1:5500/index.html' + '#' + location;
+        window.location.href = 'redfoxexteriors.ca/index' + '#' + location;
        // document.getElementById(location).scrollIntoView({behavior: "smooth", block: "center"});
     }
     burgerMenu(true)
